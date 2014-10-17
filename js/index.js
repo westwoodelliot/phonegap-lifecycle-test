eced9b9401843944a8485b06ec58a75f0c9649a7
westwoodelliot <wese1_12@uni.worc.ac.uk>
2014-10-17 11:45:18 +0100

    Added storage
    Local storage and JSON added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,27 @@
 var paused_count = 0;
 var resumed_count = 0;
 var launched_count = 0;
-
-    function onLoad() {
+var key = "name";
+var value = "bob";
+window.localStorage.setItem(key, value);
+var key = "job";
+var value = "builder";
+window.localStorage.setItem(key, value);
+var key = "fixit?";
+var value = "yes";
+window.localStorage.setItem(key, value);
+var key = "name";
+var value = window.localStorage.getItem(key);
+var length = window.localStorage.length;
+var key = "chocbar";
+var value = [{"Calories": "2270"}, {"Joules":"544"}, {"Protein":"6.6"}, {"Carbohydrates":"56.1"}];
+window.localStorage.setItem(key, value);
+var key = "bars";
+var value = [{"Name":"Galaxy"}, {"Name":"Cadburys"}, {"Name":"Mars"}];
+window.localStorage.setItem(key, value);
+    
+	
+	function onLoad() {
         document.addEventListener("deviceready", onDeviceReady, false);
 		console.log("device ready");
     }
@@ -39,3 +58,30 @@ var launched_count = 0;
 		resumed_count++;
 		updateDisplay();
     }
+	function bobDisplay()
+	{
+		alert(window.localStorage.getItem("job") + "\n" + length);
+	}
+	function chocBar()
+	{
+		alert("CHOCOLATE!");
+		window.localStorage.getItem(key);
+		document.getElementById("cal").innerHTML = value[0].Calories;
+		document.getElementById("jou").innerHTML = value[1].Joules;
+		document.getElementById("pro").innerHTML = value[2].Protein;
+		document.getElementById("car").innerHTML = value[3].Carbohydrates;
+	}
+	function dynDisp()
+	{
+		window.localStorage.getItem(key);
+		var output = "";
+		console.log(value.length);
+		for (var i=0; i<key.length-1; i++)
+		{
+			console.log(i + ", " + value[i]);
+			output += "<li>" + value[i].Name + "</li>";
+			document.getElementById("test").innerHTML = output;
+			
+		}
+		
+	}
